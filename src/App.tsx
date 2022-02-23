1de74e8f16d602733b07dcc88636060a1b969af3
Ananda Reis <ananda.queiroz70@hotmail.com>
2022-02-23 15:19:30 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import { Repo } from "./pages/Repo";
+import { Repos } from "./pages/Repos";
+
+export function App(){
+  return(
+    <Routes>
+      <Route path="/" element={<Repos/>} />
+      <Route path="/repo/*" element={<Repo/>} />
+    </Routes>
+  )
+}
